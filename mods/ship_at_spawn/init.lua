e69f4c568a139cf894e955987f20c48e57d89e45
lefty-studios <lefty-studios@users.noreply.github.com>
2019-12-09 14:19:16 -0500

    update pod schematic

--- mods/ship_at_spawn/init.lua
+++ mods/ship_at_spawn/init.lua
@@ -13,6 +13,21 @@ minetest.register_node(":water:chest_air", {
     drop = "",
     groups = {not_in_creative_inventory=1}
 })
+minetest.register_node(":water:charger_air", {
+	description = "Hacker",
+    drawtype = "airlike",
+    paramtype = "light",
+    sunlight_propagates = true,
+
+    walkable     = false, -- Would make the player collide with the air node
+    pointable    = false, -- You can't select the node
+    diggable     = false, -- You can't dig the node
+    buildable_to = true,  -- Nodes can be replace this node.
+                          -- (you can place a node and remove the air node
+                          -- that used to be there)
+    drop = "",
+    groups = {not_in_creative_inventory=1}
+})
 
 math.randomseed(os.time())
 minetest.register_abm({
@@ -24,8 +39,22 @@ minetest.register_abm({
 		local meta = minetest.get_meta(pos)
 		local inv = meta:get_inventory()
 		inv:add_item("main", "phaser:battery "..math.random(1, 3))
-		inv:add_item("main", "gunslinger:phaser")
-		inv:add_item("main", "3d_armor:helmet_scuba")
+		--inv:add_item("main", "gunslinger:phaser")
+		--inv:add_item("main", "3d_armor:helmet_scuba")
+	end,
+})
+
+minetest.register_abm({
+	nodenames = {"water:charger_air"},
+	interval = 1,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		minetest.set_node(pos, {name="water:charger", param2 = minetest.dir_to_facedir({x=0,y=0,z=1})})
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		--inv:add_item("src", "phaser:battery "..math.random(1, 3))
+		inv:add_item("src", "gunslinger:phaser")
+		--inv:add_item("main", "3d_armor:helmet_scuba")
 	end,
 })
 
@@ -39,8 +68,8 @@ minetest.register_on_joinplayer(function(player)
 		output:write("true")
 		io.close(output)
 		input = io.open(minetest.get_worldpath().."/hasPlayerJoined","r")
-		local pos_below = {x=player:get_pos().x-2, y=player:get_pos().y-1, z=player:get_pos().z-4}
-		minetest.place_schematic(pos_below, minetest.get_modpath("ship_at_spawn") .. "/schematics/pod.mts", 360, {["default:chest"] = "water:chest_air"}, true)
+		local pos_below = {x=player:get_pos().x-2, y=player:get_pos().y-2, z=player:get_pos().z-4}
+		minetest.place_schematic(pos_below, minetest.get_modpath("ship_at_spawn") .. "/schematics/pod_1.mts", 360, {["water:charger"] = "water:charger_air",["default:chest"] = "water:chest_air"}, true)
 		io.close(input)
 	else
 		--minetest.chat_send_all("Player has already joined for first time ")
